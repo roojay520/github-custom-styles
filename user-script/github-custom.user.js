d276bbd67dde92393bc8d29e25e9d169c4bef9da
roojay <roojay520@gmail.com>
2020-07-31 18:22:53 +0800

    chore: update version

--- user-script/github-custom.user.js
+++ user-script/github-custom.user.js
@@ -3,7 +3,7 @@
 // @name         github-custom-styles
 // @namespace    github-custom-styles
 // @description  github custom styles
-// @version      1.1.5
+// @version      1.1.6
 // @author       roojay <roojay520@gmail.com>
 // @homepage     https://github.com/roojay520/github-custom-styles
 // @license      MIT
